3e6f69aacb68c7c58260e23846dd57f23a7319d3
Omar junior <omarhernandez041204@gmail.com>
2022-03-12 12:07:32 -0500

    ultimos commits actualizados

--- blog/applications/entrada/models.py
+++ blog/applications/entrada/models.py
@@ -1,5 +1,8 @@
+from datetime import timedelta, datetime
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
+
 from model_utils.models import TimeStampedModel
 from ckeditor_uploader.fields import RichTextUploadingField
 
@@ -41,6 +44,7 @@ class Entry(TimeStampedModel):
     image = models.ImageField(verbose_name="Imagen", upload_to = "Entry")
     portada = models.BooleanField(default=False)
     in_home = models.BooleanField(default=False)
+    #trabajar con ulrs generadas automaticamente
     slug = models.SlugField(editable=False, max_length=300)
 
     objects = EntryManager()
@@ -51,4 +55,17 @@ class Entry(TimeStampedModel):
 
     def __str__(self):
         return self.title
-    
+
+    def save(self, *args, **kwargs):
+        #calculamos el total de segundos de la hora actual
+        now = datetime.now()        
+        total_time = timedelta(
+            hours=now.hour,
+            minutes = now.minute,
+            seconds = now.second
+        )
+        seconds = int(total_time.total_seconds())
+        slug_unique = '%s %s' % (self.title, str(seconds))
+        self.slug = slugify(slug_unique)
+        
+        super(Entry, self).save(*args, **kwargs)
--- blog/applications/entrada/urls.py
+++ blog/applications/entrada/urls.py
@@ -5,5 +5,5 @@ app_name = "entrada_app"
 
 urlpatterns = [
     path('entradas/', views.EntryListView.as_view(), name='entry-list'),
-    path('entrada/<pk>/', views.EntryDetailView.as_view(), name="entry-detail"),
+    path('entrada/<slug>/', views.EntryDetailView.as_view(), name="entry-detail"),
 ]
